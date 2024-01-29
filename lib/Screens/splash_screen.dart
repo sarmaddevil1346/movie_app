063cc87e0e384981c3a46d8ae7f7f9a85399e8b9
Muhammad Sarmad Nadeem <sarmadofficial1366@gmail.com>
2024-01-29 10:02:20 +0500

    Initial commit

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/splash_background.png"))),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("assets/images/splash_black_shadow.png"),
+            ),
+          ),
+          child: SafeArea(
+            child: Center(
+              child: Image.asset("assets/images/studio_logo.png"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
